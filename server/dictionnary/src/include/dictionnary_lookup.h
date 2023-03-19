b32df7395e389c664e14d9819f76c9c9922b5163
Léon <93096590+Leon-ED@users.noreply.github.com>
2023-03-19 10:19:25 +0100

    Delete server/dictionnary directory

--- server/dictionnary/src/include/dictionnary_lookup.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include "libs.h"
-
-#define FOUND 0
-#define NOT_FOUND 2
-#define FOUND_PREFIX 1
-int dictionnary_lookup(FILE *dico_lex, char word[]);
--- server/dictionnary/src/include/grid_build.h
+++
@@ -1,9 +0,0 @@
-/**
- *@brief Relie une lettre à son nombre d'occurence
- **/
-struct Letter_occurence
-{
-    char letter[2];
-    float count;
-};
-typedef struct Letter_occurence Letter_occurence;
