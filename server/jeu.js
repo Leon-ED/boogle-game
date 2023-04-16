1b44ddf315f4df81d39aa8bbde09e3bdc0dd0226
= <=>
2023-04-16 10:48:07 +0200

    On teste si un mot est valide avant de lancer la recherche avec le moteur du jeu

--- server/jeu.js
+++ server/jeu.js
@@ -3,19 +3,19 @@ const CWD = process.env.CWD;
 
 
 
-getGrille = function (req, res, next)  {
+getGrille = function (req, res, next) {
     const exec = require('child_process').exec;
-    const lignes = Math.max(2,req.params.lignes);
-    const colonnes = Math.max(2,req.params.colonnes);
-    
-    exec('cd '+CWD+'/bin && ./grid_build ../utils/frequences.txt ' + lignes + ' ' + colonnes, (err, stdout, stderr) => {
+    const lignes = Math.max(2, req.params.lignes);
+    const colonnes = Math.max(2, req.params.colonnes);
+
+    exec('cd ' + CWD + '/bin && ./grid_build ../utils/frequences.txt ' + lignes + ' ' + colonnes, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return res.status(400).json({
                 status: 'error',
                 message: 'Une erreur est survenue lors de la recherche.'
             });
-            
+
         }
         if (stderr) {
             console.error(stderr);
@@ -23,7 +23,7 @@ getGrille = function (req, res, next)  {
                 status: 'error',
                 message: 'Une erreur est survenue lors de la recherche.'
             });
-            
+
         }
         return res.status(200).json({
             status: 'success',
@@ -32,29 +32,42 @@ getGrille = function (req, res, next)  {
             lignes: lignes,
             colonnes: colonnes
         });
-        
+
     });
 
 
 
+}
+
+
+function preVerifMot(mot,lignes,colonnes){
+
+    if (mot.length > lignes * colonnes)
+        return false;
+    if (mot.length < 2)
+        return false;
+    if (!/^[a-zA-Z]+$/.test(mot))
+        return false;
+    return true;
 }
 
 
 verifMot = function (req, res, next) {
-    console.log('Recherche de définitions pour le mot ' + req.body.mot);
-    const exec = require('child_process').exec;
-    const {mot, grille, lignes, colonnes, langue} = req.body;
 
-    const cmd = 'cd '+CWD+'/bin && ./grid_path ' + mot.toUpperCase() + ' '+ lignes + ' ' + colonnes + ' ' + grille;
+    const exec = require('child_process').exec;
+    const { mot, grille, lignes, colonnes, langue } = req.body;
+    if(!preVerifMot(mot,lignes,colonnes))
+        return res.status(400).json({ status: 'error', message: 'Le mot ne peut pas être recherché.' });
+
+    const cmd = 'cd ' + CWD + '/bin && ./grid_path ' + mot.toUpperCase() + ' ' + lignes + ' ' + colonnes + ' ' + grille;
     exec(cmd, (err, stdout, stderr) => {
-        // check if the program has returned code 0
         if (err) {
             console.error(err);
             return res.status(400).json({
                 status: 'error',
                 message: 'Une erreur est survenue lors de la recherche.'
             });
- 
+
         }
         if (stderr) {
             console.error(stderr);
@@ -62,45 +75,34 @@ verifMot = function (req, res, next) {
                 status: 'error',
                 message: 'Une erreur est survenue lors de la recherche.'
             });
- 
+
         }
- 
+
     });
 
     const exec2 = require('child_process').exec;
-    const cmd2 =  'cd '+CWD+'/bin && ./dictionnary_lookup ../utils/dico_fr.lex ' + mot.toUpperCase();
-    exec2(cmd2, (err, stdout, stderr) => {
-        // check if the program has returned code 0
-        if (err) {
-            console.error(err);
+    const cmd2 = 'cd ' + CWD + '/bin && ./dictionnary_lookup ../utils/dico_fr.lex ' + mot.toUpperCase();
+    try {
+        exec2(cmd2, (err, stdout, stderr) => {
+
+            if (stdout == "0")
+                return res.status(200).json({
+                    status: 'success',
+                    message: 'Recherche réussie.',
+                });
             return res.status(400).json({
                 status: 'error',
-                message: 'Une erreur est survenue lors de la recherche.'
+                message: 'Recherche réussie.',
             });
-      
-        }
-        if (stderr) {
-            console.error(stderr);
-            return res.status(400).json({
-                status: 'error',
-                message: 'Une erreur est survenue lors de la recherche.'
-            });
-      
-        }
-        if(stdout == "0")
-            return res.status(200).json({
-            status: 'success',
-            message: 'Recherche réussie.',
         });
-        return res.status(400).json({
-            status: 'error',
-            message: 'Recherche réussie.',
-        });
-    });
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
 
-    
+
 
 
 
