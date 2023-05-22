6c88919d530856e4ae8f54031aa458c06270d4fc
= <=>
2023-05-22 14:09:51 +0200

    push

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,7 +1,8 @@
-export const FRONT_HOST="http://localhost:8080/";
-const BACK_END="localhost";
-const BACK_END_PORT=3000;
+export const FRONT_HOST="http://mac.local:8080";
+const BACK_END="mac.local";
+const BACK_END_PORT="3000";
 const BACK_END_PATH="/api"
 export const BACKEND_URL="http://"+BACK_END+":"+BACK_END_PORT+BACK_END_PATH;
-export const WS_URL="ws://"+BACK_END+":"+BACK_END_PORT;
+export const WS_URL="ws://"+BACK_END+":"+BACK_END_PORT+"/chat";
+export const MP_WS_URL="ws://"+BACK_END+":"+BACK_END_PORT+"/mp";
 
